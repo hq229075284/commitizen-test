6d6f01abcf7390d534a2dba6c4e7a1535a8ac520
hanqing <229075284@qq.com>
2021-10-19 00:52:11 +0800

    feature: 1

---
+++ commitlint.config.js
@@ -0,0 +1,8 @@
+const Configuration = {
+    // https://github.com/vivaxy/vscode-conventional-commits#supported-commitlint-rules
+    rules: {
+        'type-enum': [2, 'always', ['this.is.from.commitlint.config.js','2']],
+    },
+}
+
+module.exports = Configuration;
